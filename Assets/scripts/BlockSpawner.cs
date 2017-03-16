9a7a85f16264e18c3f9082de9e6bfe916359c0ab
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-03-16 11:50:07 +0000

    Spawn wave of blocks

---
+++ Assets/scripts/BlockSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+
+    [SerializeField]
+	private Transform [] spawnPoints;
+    [SerializeField]
+    private GameObject blockPrefab;
+
+    private void Start ()
+    {
+        SpawnWave ();
+    }
+
+    private void SpawnWave ()
+    {
+        int randomIndex = Random.Range (0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (i != randomIndex)
+            {
+                GameObject newBlock = Instantiate (blockPrefab, spawnPoints [i].transform.position, Quaternion.identity) as GameObject;
+            }
+        }
+    }
+
+}
