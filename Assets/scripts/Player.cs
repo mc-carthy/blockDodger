2c0a0d92e1f8a1dc65797ce84ff2e0af3f071b15
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-03-16 11:38:30 +0000

    Implement basic horizontal movement

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody2D))]
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private float moveSpeed = 15f;
+    private float screenWidth = 5f;
+    private float h;
+
+    private void Awake ()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+        rb.isKinematic = true;
+    }
+
+    private void Update ()
+    {
+        h = Input.GetAxis ("Horizontal");
+    }
+
+	private void FixedUpdate ()
+    {
+        Vector2 newPosition = rb.position + new Vector2 (h, 0) * moveSpeed * Time.fixedDeltaTime;
+
+        newPosition.x = Mathf.Clamp (newPosition.x, -screenWidth, screenWidth);
+
+        rb.MovePosition (newPosition);
+    }
+
+}
